b3dfe55acd36b37be994643f6580a1dcca2f44d6
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-15 12:29:23 +0100

    😎Feat: Follow the instructions found in the Menu.js file to complete the JavaScript functionality. NOTE: Start near the bottom of the page by looking for const menu.
     - Toggle the "menu--open" class on your menu reference.
     - Create a reference to the ".menu" class
     - Create a reference to the ".menu-button" class
     - Using your menuButton reference, add a click handler that calls toggleMenu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,14 @@
 
-const toggleMenu = () => {
+const toggleMenu = (element) => {
   // Toggle the "menu--open" class on your menu refence. 
+  element.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', () => {
+  return toggleMenu(menu);
+});
